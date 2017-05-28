6fd6dddf9e833b524b12b900e331a83dfddfc721
candy-Tong <563378816@qq.com>
2017-05-28 20:45:25 +0800

    完成私厨详情，下一步我的订单

--- page/PrivateKitchenDetails/index.js
+++ page/PrivateKitchenDetails/index.js
@@ -2,55 +2,30 @@
 var TD = require('../../utils/tdweapp');
 Page({
   data: {
-    chef_detail:{},
-    "cover": "http://img95.699pic.com/photo/00037/1338.jpg_wh300.jpg",
-    "kitchen_info": {
-      "avatar": "http://homeal.com.hk/assets/img/cook/cook_1.jpg",
-      "name": "小路私房菜",
-      "desc": "会做各种潮汕美食",
-      "tag": [
-        "潮汕美食"
-      ]
-    },
-    "navigate":
-    [{
-      "name": "推荐菜单",
-      "num": 66
-    }, {
-      "name": "自选菜单",
-      "num": 371
-    }, {
-      "name": "用户评价",
-      "num": 195
-    }
-    ],
-    "currentTag": "recommend-menu"
+    tabs: ["套餐", "信息", "用户评价"],
+    activeIndex: 0,
+    sliderOffset: 0,
+    sliderLeft: 0,
+    chef_detail: {},
   },
-  setCurrentTag(event) {
-    let index = event.currentTarget.dataset.index;
-    console.log(index);
-    switch (index) {
-      case 0:
-        this.setData({ currentTag: "recommend-menu" });
-        break;
-      case 1:
-        this.setData({ currentTag: "optional-menu" });
-        break;
-      case 2:
-        this.setData({ currentTag: "rating" });
-        break;
-      default:
-        console.log("fail to setCurrentTag,wrong index");
-        break;
-    }
+
+  /**
+   * tab切换
+   */ 
+  tabClick: function (e) {
+    this.setData({
+      sliderOffset: e.currentTarget.offsetLeft,
+      activeIndex: e.currentTarget.id
+    });
   },
+
   onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
-    let _this=this;
+    let _this = this;
     wx.request({
       url: 'http://homeal.com.hk/api/chefdetails_rest/chef',
       data: {
-        chef_id:1
+        chef_id: 1
       },
       header: {
         'content-type': 'application/json'
@@ -68,7 +43,7 @@ Page({
   },
   onShow: function () {
     // 页面显示
-    TD.Page.load(true); // 此时，需要在TD.Page.load()中设置参数true，用于说明此时为Tabs方式；
+
 
   },
   onHide: function () {
